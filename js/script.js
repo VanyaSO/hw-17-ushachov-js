6a390f23032b19e635afaeff8bd8962cfcae373e
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-04-10 02:25:06 +0300

    й

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
 'use strict'
 
-const number = +prompt('Ведите число');
-let isSimple = true;
-
-if(number === 1) {
-    console.log(false)
-} else if(number > 1) {
-
-
-    for (let i = 2; i < number; i++){
-        if(number % i === 0){
-            isSimple = false;
-            break;
-        }
-    }
-    console.log(isSimple)
-
-}
-
 
